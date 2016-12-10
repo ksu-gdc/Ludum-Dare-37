5ce4a3ad6ce5cfffadad1c60d74fa951ed191845
daytonltaylor <daytonltaylor@outlook.com>
2016-12-10 15:11:05 -0600

    update player controller script

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -4,32 +4,32 @@ using System.Collections;
 public class Player_Controller : MonoBehaviour
 {
     public float speed;
+    public const float stopAnimation = 0.5f;
     private Rigidbody2D rb2d;
-    //private Quaternion prevRot;
-    private float prevAngle;
-    Vector3 forward;
+    private Animator animator;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        forward = transform.forward;
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 move = new Vector2(moveHorizontal, moveVertical);
+        if ((move.x > stopAnimation || move.x < stopAnimation) || (move.y > stopAnimation || move.x < stopAnimation))
+        {
+
+        }
+       // else animator.Blend("player_character_0",.5f);
+        rb2d.AddForce(move * speed);
         Vector2 moveDirection = rb2d.velocity;
         if (moveDirection != Vector2.zero)
         {
             float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
         }
-    }
-
-    void FixedUpdate()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 move = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(move * speed);
     }
 }
