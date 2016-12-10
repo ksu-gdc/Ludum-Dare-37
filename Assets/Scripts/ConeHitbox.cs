5cb703208be54cbc7a0b56b87526630e2ab844b4
NickABoen <NickABoen@gmail.com>
2016-12-10 15:37:56 -0600

    Add script to resize cone hitbox freely

--- Assets/Scripts/ConeHitbox.cs
+++ Assets/Scripts/ConeHitbox.cs
@@ -1,36 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ConeDetector : MonoBehaviour {
+public class ConeHitbox: MonoBehaviour {
     public GameObject HitBoxPrefab;
     public float ShortWidth, LongWidth, Length;
     private GameObject HitBox;
     private CircleCollider2D bulb_collider;
     private PolygonCollider2D cone;
 
-    void Awake()
+    void Start()
     {
         HitBox = Instantiate<GameObject>(HitBoxPrefab);
+        HitBox.transform.SetParent(gameObject.transform, false);
         bulb_collider = HitBox.GetComponent<CircleCollider2D>();
         cone = HitBox.GetComponent<PolygonCollider2D>();
         if(bulb_collider == null) 
         {
+            Destroy(HitBox);
             throw new System.Exception("Could not locate the circle collider");
         }
         if(cone == null) 
         {
+            Destroy(HitBox);
             throw new System.Exception("Could not locate the polygon collider");
         }
-        Destroy(HitBox);
+        UpdateSize();
     }
 
     void OnValidate()
     {
-        bulb_collider.offset += Vector2.right * Length;
+        if(HitBox != null)
+        {
+            UpdateSize();
+        }
+    }
+
+    void UpdateSize()
+    {
+        bulb_collider.offset = Vector2.right * Length;
         bulb_collider.radius = LongWidth / 2;
-        cone.points[0] = Vector2.up * (ShortWidth / 2);
-        cone.points[1] = new Vector2(Length, LongWidth / 2);
-        cone.points[2] = new Vector2(Length, -1 * cone.points[1].y);
-        cone.points[3] = cone.points[1] * -1;
+
+        Vector2[] points = cone.GetPath(0);
+        points[0] = Vector2.up * (ShortWidth / 2);
+        points[1] = new Vector2(Length, LongWidth / 2);
+        points[2] = new Vector2(Length, -1 * (LongWidth/2));
+        points[3] = cone.points[0] * -1;
+        cone.SetPath(0, points);
     }
 }
