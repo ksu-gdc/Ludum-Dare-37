cd7a9ea70f85fb866b5d11b396c0050da0a056b0
daytonltaylor <daytonltaylor@outlook.com>
2016-12-10 13:56:30 -0600

    move with WASD with added rotation and drag for stopping

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -5,20 +5,31 @@ public class Player_Controller : MonoBehaviour
 {
     public float speed;
     private Rigidbody2D rb2d;
+    //private Quaternion prevRot;
+    private float prevAngle;
+    Vector3 forward;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        forward = transform.forward;
     }
 
-	void FixedUpdate()
+    void Update()
+    {
+        Vector2 moveDirection = rb2d.velocity;
+        if (moveDirection != Vector2.zero)
+        {
+            float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
+        }
+    }
+
+    void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        Vector3 forward = transform.forward;
-        Vector3 headingAngle = Quaternion.LookRotation(forward).eulerAngles;
         Vector2 move = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce(move * speed);
-        transform.Rotate; 
     }
 }
