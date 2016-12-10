5d191761e79c3357b655fe9a742ae397f5be1788
NickABoen <NickABoen@gmail.com>
2016-12-10 02:57:56 -0600

    Create detected script to manage response functions

---
+++ Assets/Scripts/Detected.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class Detected : MonoBehaviour {
+
+    public UnityEvent OnEnter, OnStay, OnExit;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        OnEnter.Invoke();
+    }
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        OnStay.Invoke();
+    }
+
+    void OnCollisionExit2D(Collision2D coll)
+    {
+        OnExit.Invoke();
+    }
+}
