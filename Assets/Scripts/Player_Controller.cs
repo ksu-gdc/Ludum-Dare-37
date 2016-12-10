593beac8dc57e44aa1b07370754d2d6814d1aef1
daytonltaylor <daytonltaylor@outlook.com>
2016-12-10 00:47:59 -0600

    update player movement controller

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var moveY = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        transform.Rotate(0, moveX, 0);
+        transform.Translate(0, 0, moveY);	
+	}
+}
