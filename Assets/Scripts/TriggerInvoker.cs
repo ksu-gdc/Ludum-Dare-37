f4369c105395680763789d56f62c4ba6489ede6c
NickABoen <NickABoen@gmail.com>
2016-12-10 13:51:29 -0600

    Allow Trigger Invoker to work with compound triggers

--- Assets/Scripts/TriggerInvoker.cs
+++ Assets/Scripts/TriggerInvoker.cs
@@ -1,24 +1,67 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.Events;
+using System.Collections.Generic;
 
 public class TriggerInvoker: MonoBehaviour {
-
     public UnityEvent OnEnter, OnStay, OnExit;
 
-    void OnTriggerEnter2D(Collider2D coll)
+    private int collision_count = 0;
+
+    private void AddCollision()
     {
-        Debug.Log("Trigger Collision entered");
+        collision_count++;
+    }
+
+    private void RemoveCollision()
+    {
+        collision_count = Mathf.Max(0, collision_count - 1);
+    }
+
+    public bool IsColliding()
+    {
+        return collision_count > 0;
+    }
+
+    public bool EnteredCollision()
+    {
+        return collision_count == 1;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        AddCollision();
+        if (EnteredCollision())
+            OnCompoundTriggerEnter(other);
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (IsColliding())
+            OnCompoundTriggerStay(other);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        RemoveCollision();
+        if (!IsColliding())
+            OnCompoundTriggerExit(other);
+    }
+
+    void OnCompoundTriggerEnter(Collider2D other)
+    {
+        Debug.Log("Compound Trigger Enter");
         OnEnter.Invoke();
     }
 
-    void OnTriggerStay2D(Collider2D coll)
+    void OnCompoundTriggerStay(Collider2D other)
     {
         OnStay.Invoke();
     }
 
-    void OnTriggerExit2D(Collider2D coll)
+    void OnCompoundTriggerExit(Collider2D other)
     {
+        Debug.Log("Compound Trigger Exit");
         OnExit.Invoke();
     }
 }
