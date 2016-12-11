854ee88f6676e0edab3dc540d72d402175003425
NickABoen <NickABoen@gmail.com>
2016-12-11 00:02:26 -0600

    Add Image Effect skeleton for Dynamic Lighting

---
+++ Assets/Shaders/Dynamic Lighting/DynamicLightingIEScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DynamicLightingIEScript : MonoBehaviour {
+
+    public Material EffectMaterial;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        Graphics.Blit(src, dst, EffectMaterial);
+    }
+}
